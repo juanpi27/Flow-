b44e40f19b342d60aaf7a8a6c44b92b1d0db061e
juanpi27 <juanpi27@users.noreply.github.com>
2016-03-27 12:44:55 -0400

    Added files via upload

---
+++ flow/Init.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package flow;
+
+import java.io.File;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author Juan
+ */
+public class Init {
+    public void Begin(){
+        System.out.println("Starting now");
+        String method = "c";
+        Scanner in = new Scanner(System.in); 
+        System.out.println("Please select the method to be used:");
+        while(!method.equals("a") && !method.equals("b")){
+            System.out.println("a Input File \nb Generate Code "); 
+            method = in.nextLine();
+            switch (method) {
+                case "a":
+                    new LexicalAnalyzer().start();
+                    break;
+                case "b":
+                    new LexicalAnalyzer().start2();
+                    break;
+                default:
+                    System.out.println("Please enter a or b");
+                    break;
+            }
+        } 
+
+    }
+}
---
+++ flow/LexicalAnalyzer.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package flow;
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Juan
+ */
+public class LexicalAnalyzer {
+    
+    public void start(){
+        Scanner in = new Scanner(System.in); 
+	String filename;
+        String filename2;
+        System.out.println("Enter filename(place the .fpp in project folder, dont write .fpp): "); 
+        filename = in.nextLine(); 
+        System.out.println("Enter the name of the file that will be generated(do not include extension): "); 
+        System.out.println("(.txt file will be placed in project folder)"); 
+        filename2 = in.nextLine(); 
+        File f = new File(filename+".fpp");
+        Reading rf = new Reading(f);
+        Lexical lex = new Lexical(rf.readFile());
+        Output ou = new Output(filename2);
+        ou.fileWritter(lex.LexicalAnalyzer());
+        System.out.print("Ending... "); 
+    }
+    
+    public void start2(){
+        Scanner in = new Scanner(System.in);  
+        new Output("test").fileWritterfpp(new CodeGenerator().Generate());  
+        Reading rf = new Reading(new File("test.fpp")); 
+        new Output("output").fileWritter(new Lexical(rf.readFile()).LexicalAnalyzer());
+        System.out.print("Ending... ");   
+    }
+    
+}
